7694778faa697838bc2ab8ab607ed4f1d97ade4f
mariomui <yi.kan.mui@gmail.com>
2022-11-05 19:28:20 -0700

    Initial previewer.
    Move prism files to public folder.
    
    Create test file.
    
    Add check.
    
    Latest.
    
    Fix line.
    
    Remove math.
    
    Sync hcanges.

--- renderer/src/App.svelte
+++ renderer/src/App.svelte
@@ -1,25 +1,25 @@
 <script>
-    import Workspace from "./components/workspace/Workspace.svelte";
-    import monokai from "@/assets/themes/monokai.json";
-    import tomorrowNight from "@/assets/themes/tomorrow-night.json";
+  import Workspace from './components/workspace/Workspace.svelte';
+  import monokai from '@/assets/themes/monokai.json';
+  import tomorrowNight from '@/assets/themes/tomorrow-night.json';
 
-    let editorThemes = [
-        { name: "monokai", data: monokai },
-        { name: "tomorrow-night", data: tomorrowNight },
-    ];
+  let editorThemes = [
+    { name: 'monokai', data: monokai },
+    { name: 'tomorrow-night', data: tomorrowNight },
+  ];
 </script>
 
 <main
-    id="app"
-    class="flex flex-col w-screen h-screen"
-    style=" --editorBackground: {monokai.colors['editor.background']}"
+  id="app"
+  class="flex flex-col w-screen h-screen"
+  style=" --editorBackground: {monokai.colors['editor.background']}"
 >
-    <Workspace {editorThemes} />
+  <Workspace {editorThemes} />
 </main>
 
 <style>
-    #app {
-        height: 100vh;
-        width: 100vw;
-    }
+  #app {
+    height: 100vh;
+    width: 100vw;
+  }
 </style>
---
+++ renderer/src/components/workspace/temp-test.js
@@ -0,0 +1,11 @@
+export const test_markdown_checkbox = `\
+* title
+  * [ ] mario = me\n
+  * [ ] do other shit\n
+  * [ ] do other shit\n
+
+\`\`\`js
+console.log('mariosss')
+\`\`\`
+
+`;
