f577d241ba1048b97c4c6945817b5c1df60b6ccc
Alucard <minxter.yt@gmail.com>
2022-11-06 06:27:09 +0000

    Css reset

--- renderer/src/components/renderer/renderer.svelte
+++ renderer/src/components/renderer/renderer.svelte
@@ -1,60 +1,60 @@
 <script>
-  import markdownit from 'markdown-it';
-  import { beforeUpdate } from 'svelte';
-  import mdTaskLists from 'markdown-it-task-lists';
-  export let value;
+    import markdownit from "markdown-it";
+    import { beforeUpdate } from "svelte";
+    import mdTaskLists from "markdown-it-task-lists";
+    export let value;
 
-  let wrapper;
-  let parsedHtml = '';
+    let wrapper;
+    let parsedHtml = "";
 
-  if (window && window.Prism) {
-    window.Prism.manual = true;
-  }
-  const defaultOptions = {
-    langPrefix: 'language-',
-    html: true,
-    linkify: true,
-    typographer: true,
-    highlight: handleWithPrePackedPrism,
-  };
-
-  function handleWithPrePackedPrism(code, lang) {
-    if (!window.Prism) {
-      return code;
+    if (window && window.Prism) {
+        window.Prism.manual = true;
     }
-    try {
-      const highlighted_html = window.Prism.highlight(
-        code,
-        window.Prism.languages[lang],
-        lang
-      );
-      window.Prism.highlightAll();
+    const defaultOptions = {
+        langPrefix: "language-",
+        html: true,
+        linkify: true,
+        typographer: true,
+        highlight: handleWithPrePackedPrism,
+    };
 
-      return highlighted_html;
-    } catch (err) {
-      console.log({ err, message: 'Grammar Not Found' });
-      return window.Prism.util.encode(code);
+    function handleWithPrePackedPrism(code, lang) {
+        if (!window.Prism) {
+            return code;
+        }
+        try {
+            const highlighted_html = window.Prism.highlight(
+                code,
+                window.Prism.languages[lang],
+                lang
+            );
+            window.Prism.highlightAll();
+
+            return highlighted_html;
+        } catch (err) {
+            console.log({ err, message: "Grammar Not Found" });
+            return window.Prism.util.encode(code);
+        }
     }
-  }
 
-  const md = markdownit(defaultOptions);
-  md.use(mdTaskLists, { label: true, enabled: true, labelAfter: true });
+    const md = markdownit(defaultOptions);
+    md.use(mdTaskLists, { label: true, enabled: true });
 
-  beforeUpdate(() => {
-    parsedHtml = md.render(value ?? '<div>Not Working</div>');
-  });
+    beforeUpdate(() => {
+        parsedHtml = md.render(value ?? "<div>Not Working</div>");
+    });
 </script>
 
-<main class="bg-base-300 h-full w-full relative line-numbers overflow-auto">
-  <div>
+<main class="bg-base-300 h-full w-full relative line-numbers overflow-auto p-2">
     <div>
-      {#if parsedHtml}
-        <div bind:this={wrapper}>
-          {@html parsedHtml}
+        <div>
+            {#if parsedHtml}
+                <div bind:this={wrapper}>
+                    {@html parsedHtml}
+                </div>
+            {/if}
         </div>
-      {/if}
     </div>
-  </div>
 </main>
 
 <style>
--- renderer/src/components/workspace/Workspace.svelte
+++ renderer/src/components/workspace/Workspace.svelte
@@ -1,93 +1,90 @@
 <script>
-  import Editor from '../editor/Editor.svelte';
-  import Renderer from '../renderer/renderer.svelte';
-  import Toggle from '../toolbar/Toggle.svelte';
-  import Tabs from '../toolbar/Tabs.svelte';
-  import { test_markdown_checkbox } from './temp-test';
+    import Editor from "../editor/Editor.svelte";
+    import Renderer from "../renderer/renderer.svelte";
+    import Toggle from "../toolbar/Toggle.svelte";
+    import Tabs from "../toolbar/Tabs.svelte";
+    import { test_markdown_checkbox } from "./temp-test";
 
-  export let editorThemes;
+    export let editorThemes;
 
-  let viewState = 1;
-  let currentTab = 0;
-  let tabs = [];
+    let viewState = 1;
+    let currentTab = 0;
+    let tabs = [];
 
-  function createTab(tabData) {
-    tabs.push(tabData);
-  }
+    function createTab(tabData) {
+        tabs.push(tabData);
+    }
 
-  createTab({
-    name: 'test.md',
-    value: "# test\n```js\nconsole.log('test')\n```",
-  });
-  createTab({
-    name: 'test2.md',
-    value: test_markdown_checkbox,
-  });
+    createTab({
+        name: "test.md",
+        value: "# test\n```js\nconsole.log('test')\n```",
+    });
+    createTab({
+        name: "test2.md",
+        value: test_markdown_checkbox,
+    });
 
-  function setValue({ detail }) {
-    console.log(detail.value);
-    tabs[currentTab].value = detail.value;
+    function setValue({ detail }) {
+        console.log(detail.value);
+        tabs[currentTab].value = detail.value;
 
-    console.log(tabs);
-  }
+        console.log(tabs);
+    }
 
-  function viewUpdate({ detail }) {
-    viewState = detail.state;
-  }
-  function tabUpdate({ detail }) {
-    currentTab = detail.activeTab;
-  }
+    function viewUpdate({ detail }) {
+        viewState = detail.state;
+    }
+    function tabUpdate({ detail }) {
+        currentTab = detail.activeTab;
+    }
 </script>
 
 <div id="workspace">
-  <span class="flex justify-between">
-    <Tabs {tabs} on:tabUpdate={tabUpdate} />
-    <Toggle on:viewUpdate={viewUpdate} />
-  </span>
-  {#key currentTab}
-    {#if tabs.length > 0}
-      <div id="workspace-content">
-        {#if viewState === 1}
-          <Editor
-            value={tabs[currentTab].value}
-            minimap={false}
-            theme="tomorrow-night"
-            themes={editorThemes}
-            on:editorUpdate={setValue}
-          />
-        {:else if viewState === 2}
-          <Editor
-            value={tabs[currentTab].value}
-            minimap={false}
-            theme="tomorrow-night"
-            themes={editorThemes}
-            on:editorUpdate={setValue}
-          />
-          <Renderer value={tabs[currentTab].value} />
+    <span class="flex justify-between">
+        <Tabs {tabs} on:tabUpdate={tabUpdate} />
+        <Toggle on:viewUpdate={viewUpdate} />
+    </span>
+    {#key currentTab}
+        {#if tabs.length > 0}
+            <div id="workspace-content">
+                {#if viewState === 1}
+                    <Editor
+                        value={tabs[currentTab].value}
+                        minimap={false}
+                        theme="tomorrow-night"
+                        themes={editorThemes}
+                        on:editorUpdate={setValue}
+                    />
+                {:else if viewState === 2}
+                    <Editor
+                        value={tabs[currentTab].value}
+                        minimap={false}
+                        theme="tomorrow-night"
+                        themes={editorThemes}
+                        on:editorUpdate={setValue}
+                    />
+                    <Renderer value={tabs[currentTab].value} />
+                {:else}
+                    <Renderer value={tabs[currentTab].value} />
+                {/if}
+            </div>
         {:else}
-          <Renderer value={tabs[currentTab].value} />
+            <div />
         {/if}
-      </div>
-    {:else}
-      <div />
-    {/if}
-  {/key}
+    {/key}
 </div>
 
 <style>
-  #workspace {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-  #workspace-content {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-  }
-  #workspace-content {
-    transition: width 2s, height 2s, transform 2s;
-  }
+    #workspace {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    #workspace-content {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+    }
 </style>
